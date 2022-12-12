3aaad6ada6227b1c27c395685caf45fb16e320e9
Kremoweczka12 <76210730+Kremoweczka12@users.noreply.github.com>
2022-12-12 10:58:16 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -59,6 +59,59 @@ int main(int argc, char** argv) {
 
 
     auto start = std::chrono::steady_clock::now();
+    auto minor_start = std::chrono::steady_clock::now();
+    if(method=="compare"){
+        puzzle_t tab = get_random_tab(tabela_4);
+        int c = 0;
+        minor_start = std::chrono::steady_clock::now();
+        puzzle_t tab_3 = annealing(tab, iter, convergence_curve, calls_number_print);
+        if (display_solution == "yes"){std::cout << tab_3 << std::endl;}
+        std::cout << "annaeling result: " << evaluate_puzzle(tab_3, c) << std::endl;
+        auto minor_end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> elapsed_seconds = minor_end-minor_start;
+        std::cout << "finished in time: " << elapsed_seconds.count() << "s\n";
+
+        minor_start = std::chrono::steady_clock::now();
+        tab_3 = deterministic_hill_climbing(tab, iter, convergence_curve, calls_number_print);
+        minor_end = std::chrono::steady_clock::now();
+        elapsed_seconds = minor_end-minor_start;
+        std::cout << "finished in time: " << elapsed_seconds.count() << "s\n";
+        if (display_solution == "yes"){std::cout << tab_3 << std::endl;}
+        std::cout << "deterministic hill climbing result: " << evaluate_puzzle(tab_3, c) << std::endl;
+
+        minor_start = std::chrono::steady_clock::now();
+        tab_3 = stochastic_hill_climbing(tab, iter, convergence_curve, calls_number_print);
+        minor_end = std::chrono::steady_clock::now();
+        elapsed_seconds = minor_end-minor_start;
+        std::cout << "finished in time: " << elapsed_seconds.count() << "s\n";
+
+        if (display_solution_quality == "yes"){
+            std::cout << "stochastic hill climbing found solution has: " << evaluate_puzzle(tab_3, c) << " errors." << std::endl;
+        }
+        if (display_solution == "yes"){std::cout << tab_3 << std::endl;}
+
+        minor_start = std::chrono::steady_clock::now();
+        tab_3 = stochastic_method(tab, iter, convergence_curve, calls_number_print);
+        minor_end = std::chrono::steady_clock::now();
+        elapsed_seconds = minor_end-minor_start;
+        std::cout << "finished in time: " << elapsed_seconds.count() << "s\n";
+        if (display_solution_quality == "yes"){
+            std::cout << "pure stochastic method found solution has: " << evaluate_puzzle(tab_3, c) << " errors." << std::endl;
+        }
+        if (display_solution == "yes"){std::cout << tab_3 << std::endl;}
+
+        minor_start = std::chrono::steady_clock::now();
+        tab_3 = tabu_method(tab, iter, max_size, convergence_curve, calls_number_print);
+        minor_end = std::chrono::steady_clock::now();
+        elapsed_seconds = minor_end-minor_start;
+        std::cout << "finished in time: " << elapsed_seconds.count() << "s\n";
+        if (display_solution_quality == "yes"){
+            std::cout << "tabu method found solution has: " << evaluate_puzzle(tab_3, c) << " errors." << std::endl;
+        }
+        if (display_solution == "yes"){std::cout << tab_3 << std::endl;}
+
+
+    }
     if(method=="a"){
         puzzle_t tab = get_random_tab(tabela_4);
         puzzle_t tab_3 = annealing(tab, iter, convergence_curve, calls_number_print);
